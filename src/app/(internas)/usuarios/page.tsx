c4bac508f79a66b188e8840083b18d410afa125c
Alexandre-Souza-Cardoso <ale.developer72@gmail.com>
2024-11-14 17:58:00 -0300

    feat: "backend"

--- src/app/(internas)/usuarios/page.tsx
+++ src/app/(internas)/usuarios/page.tsx
@@ -1,30 +1,56 @@
 'use client'
+import Backend from "@/app/backend";
 import Pagina from "@/app/components/template/pagina";
 import Titulo from "@/app/components/template/Titulo";
 import FormularioUsuario from "@/app/components/usuario/FormularioUsuario";
-import usuarios from "@/app/data/constants/usuarios";
+import ListaUsuario from "@/app/components/usuario/ListaUsuario";
 import { Usuario } from "@/core/model/Usuario";
-import { IconUser } from "@tabler/icons-react";
-import { useState } from "react";
+import { IconPlus, IconUser } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 
 export default function Page() {
-    const [usuario, setUsuario] = useState<Usuario>(usuarios[0])
+    const [usuarios, setUsuarios] = useState<Usuario[]>([])
+    const [usuario, setUsuario] = useState<Partial<Usuario> | null>(null)
 
-    function salvar() {
-        // Salvar no banco de dados
+    useEffect(() => {
+        Backend.usuarios.obter().then(setUsuarios)
+    },[])
+
+    async function salvar() {
+        if (!usuario) return
+        await Backend.usuarios.salvar(usuario)
+        const usuarios = await Backend.usuarios.obter()
+        setUsuarios(usuarios)
+        setUsuario(null)
     }
 
     return (
         <Pagina className="flex flex-col gap-10">
             <Titulo icone={IconUser} principal="Usuários" segundario="Cadastro de usuários" />
-            {/* <ListaUsuario /> */}
-            <FormularioUsuario
-                usuario={usuario}
-                onChange={setUsuario}
-                salvar={salvar}
-                cancelar={() => {}}
-            />
+
+            {usuario ? (
+                <FormularioUsuario
+                    usuario={usuario}
+                    onChange={setUsuario}
+                    salvar={salvar}
+                    cancelar={() => setUsuario(null)}
+                />
+            ) : (
+                <>
+                    <div className="flex justify-end">
+                        <button className="flex items-center gap-2 bg-blue-500 px-4 py-2 rounded-md" onClick={() => setUsuario({})}>
+                            <IconPlus />
+                            <span>Novo Usuário</span>
+
+                        </button>
+
+                    </div>
+                    <ListaUsuario usuarios={usuarios} onClick={setUsuario} />
+                </>
+
+            )}
+
         </Pagina>
     )
 }
--- src/app/components/usuario/LinhaUsuario.tsx
+++ src/app/components/usuario/LinhaUsuario.tsx
@@ -3,23 +3,28 @@ import Image from 'next/image'
 
 export interface LinhaUsuarioProps {
     usuario: Usuario
+    onClick?: (usuario: Usuario) => void
 }
 
 export default function LinhaUsuario(props: LinhaUsuarioProps) {
     const avatarUrl = props.usuario.avatar || `https://ui-avatars.com/api/?name=${encodeURIComponent(props.usuario.nome)}&size=80&background=random`;
     return (
-        <div className="flex bg-zinc-900 items-center gap-5 p-4 rounded-md">
+        <div
+            className="flex bg-zinc-900 items-center gap-5 p-4 rounded-md cursor-pointer"
+            onClick={() => props.onClick?.(props.usuario)}
+        >
+
             <Image
                 src={avatarUrl}
                 width={50}
                 height={50}
                 className="rounded-full"
-                alt="Avatar" 
+                alt="Avatar"
             />
             <div className="flex flex-col">
                 <span className="text-xl font-bold">{props.usuario.nome}</span>
                 <span className="text-sm text-zinc-400">{props.usuario.email}</span>
             </div>
-        </div>
+        </div >
     )
 }
